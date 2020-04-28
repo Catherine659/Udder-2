810afee6dc6a9c18ef1b7330bb253b3b702f4ba5
Catherine369 <elisabeth.kilburn@gmail.com>
2020-04-28 10:57:57 +0100

    Add Label & Text

---
+++ Udder-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder-2
+//
+//  Created by user166306 on 4/28/20.
+//  Copyright © 2020 user166306. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
